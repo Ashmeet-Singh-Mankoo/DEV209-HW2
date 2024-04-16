70f1af0377d79656d992505b1ac200c858cddb5b
Ashmeet S <Ashmeet S@ASHMEETS-LAPTOP>
2024-04-15 19:40:01 -0700

    Second Commit

--- script.js
+++ script.js
@@ -1,39 +1,52 @@
-let wordsArray = [];
+let wordArray = [];
+for(let i=0; i<3; i++){
+  wordArray[i] = prompt("Please enter a word");
+}
 
-function displayWords() {
-  const list = document.getElementById('myList');
-  list.innerHTML = "";
-  wordsArray.forEach(word => {
-    const listItem = document.createElement('li');
-    listItem.textContent = word;
-    list.appendChild(listItem);
+document.addEventListener("DOMContentLoaded", function(event){
+  let ulVar = document.getElementById('myUL');
+  for(let i=0; i < wordArray.length; i++){
+    let liVar = document.createElement('li');
+    liVar.innerHTML = wordArray[i];
+    ulVar.appendChild(liVar);
+  }
+
+    document.getElementById("red").addEventListener("click", function() {
+      let div = document.getElementById("divcolor");
+      if ( div.classList.contains('black') ){
+        div.classList.remove('black');
+      }
+      div.classList.add('red');
   });
+    document.getElementById("swap").addEventListener("click", swap );
+
+})
+
+function caps(){
+  let newArray = wordArray.map(myCaps);
+  let ulVar = document.getElementById('myUL');
+  ulVar.innerHTML = "";
+  for (let i = 0; i < newArray.length; i++) {
+    let liVar = document.createElement('li');
+    liVar.innerHTML = newArray[i];
+    ulVar.appendChild(liVar);
+  };
 }
 
-function convertToCaps() {
-  var capsArray = wordsArray.map(word => word.toUpperCase());
-  displayWords(capsArray);
+function myCaps(oneItem) {
+  return oneItem.toUpperCase();
 }
 
-function changeColor() {
-  var listItems = document.querySelectorAll('#myList li');
-  listItems.forEach(item => {
-    item.style.color = 'red';
-  });
-}
+function swap() {
+  let swapperArray = wordArray.map(function(oneWord) {
+    return oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0);})
+  
 
-function swapLetters() {
-  var swappedArray = wordsArray.map(word => {
-    return word.charAt(word.length - 1) + word.substring(1, word.length - 1) + word.charAt(0);
-  });
-  displayWords(swappedArray);
+  let ulVar2 = document.getElementById('myUL');
+  ulVar2.innerHTML = "";
+  for (let i = 0; i < swapperArray.length; i++) {
+    let liVar = document.createElement('li');
+    liVar.innerHTML = swapperArray[i];
+    ulVar2.appendChild(liVar);
+  }
 }
-
-// Gather 3 words
-for (let i = 0; i < 3; i++) {
-  const word = prompt('Enter a word:');
-  wordsArray.push(word);
-}
-
-// Display the words initially
-displayWords();
